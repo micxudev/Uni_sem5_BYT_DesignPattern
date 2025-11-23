a4161acec8f21a650a46d389e672769f5ab94718
dredd <1misha.baltovsky@gmail.com>
2025-11-23 20:43:45 +0100

    Implement `Adapter Design Pattern`

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -5,9 +5,25 @@ namespace DesignPattern.Adapter
     {
         private readonly BillingSystem thirdPartyBillingSystem = new();
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            var rows = employeesArray.GetLength(0);
+
+            var listEmployee = new List<Employee>(rows);
+
+            for (var i = 0; i < rows; i++)
+            {
+                var id = int.Parse(employeesArray[i, 0]);
+                var name = employeesArray[i, 1];
+                var designation = employeesArray[i, 2];
+                var salary = decimal.Parse(employeesArray[i, 3]);
+
+                var employee = new Employee(id, name, designation, salary);
+
+                listEmployee.Add(employee);
+            }
+
+            thirdPartyBillingSystem.ProcessSalary(listEmployee);
+        }
     }
 }
